a34cf1f2102dceebbb4ed8c5bd38f3644d46f970
GogGz <120939822+GogGz@users.noreply.github.com>
2023-01-16 00:41:18 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace GUID;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Guid obj = Guid.NewGuid();
+        Console.WriteLine("Create guid: " + obj.ToString());
+
+        obj = Guid.Empty;
+
+        if (obj == Guid.Empty)
+        {
+            Console.WriteLine("guid is empty");
+        }
+
+        Console.WriteLine(obj.ToString());
+
+        string str = Guid.Empty.ToString();
+
+        Console.WriteLine("string represent empty guid: " + str);
+
+        string str1 = Guid.NewGuid().ToString();
+
+        Console.WriteLine("new guid to string: " + str1);
+
+            
+    }
+}
+
